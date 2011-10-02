e6a7ba377fcb120767c2cfcb0807326124710482
Oleksandr Bondar <obondar@gmail.com>
2011-10-02 10:07:01 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Timebank::Application.config.secret_token = '070be6653071bad00f93768120e52050a9b9403c187191eb38b6044ee86772e9ba1272956a05880c68f33adb3d8ff5547bb2e35af59706422eeb6e2b2c8b517c'
